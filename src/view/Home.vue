ac0dffa94824c9fef3e1a3b0ef42bd4afa5e56eb
Wang <tp953704@gmail.com>
2020-10-14 09:28:25 +0800

    vuex

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,28 +1,24 @@
 <template>
-  <div class="Home">
-    <TheaterHeader :Scence="[...ScenceManger]" v-if="isShow == 'Scence'" />
+<div class="Home">
+    <TheaterHeader :Scence="[this.getCurrentData]" v-if="isShow == 'Scence'" />
     <div class="theater theater__pcCover">
-      <div class="theater__main">
-        <transition name="trans" mode="out-in">
-          <Loading v-if="$store.state.isLoading" />
-        </transition>
-        <transition name="fade" mode="out-in">
-          <Opening v-if="isShow == 'Opening'" @emitOpeningBtn="openingBtn" />
-        </transition>
-        <transition name="fade" mode="out-in">
-          <Scence v-if="isShow == 'Scence'" :Scence="[...ScenceManger]" />
-        </transition>
-      </div>
-      <Procedure />
-      <TheaterOptions
-        :Scence="[...ScenceManger]"
-        @next="nextScence"
-        v-if="isShow == 'Scence'"
-      />
+        <div class="theater__main">
+            <transition name="trans" mode="out-in">
+                <Loading v-if="$store.state.isLoading" />
+            </transition>
+            <transition name="fade" mode="out-in">
+                <Opening v-if="isShow == 'Opening'" @emitOpeningBtn="openingBtn" />
+            </transition>
+            <transition name="fade" mode="out-in">
+                <Scence v-if="isShow == 'Scence'" :Scence="[this.getCurrentData]" />
+            </transition>
+        </div>
+        <Procedure />
+        <TheaterOptions :Scence="[this.getCurrentData]" @next="nextScence" v-if="isShow == 'Scence'" />
     </div>
     <!-- <div class="" :Scence="[...ScenceManger]"></div> -->
     <!-- <button @click="ani">12</button> -->
-  </div>
+</div>
 </template>
 
 <script>
@@ -32,106 +28,124 @@ import Opening from "@/components/Opening.vue";
 import Scence from "@/components/Scence.vue";
 import Procedure from "@/components/Theater/Procedure.vue";
 import TheaterOptions from "@/components/Theater/TheaterOptions.vue";
-import { SceneManager } from "@/plugins/scenceFactory.js";
-import { scenesAll } from "@/static/json/scenes.js";
-// import Lottie from "@/components/Lottie.vue";
-// import * as animationData from "../static/home/home.json";
-// import * as animation2 from "../static/foreword/rocket.json";
-export default {
-  name: "Home",
-  data() {
-    return {
-      ScenceManger: {},
-      isShow: "Opening",
-      // 測試
-      // isShow: "Scence",
-      isTranform: false
-      //   defaultOptions: {
-      //     animationData: animationData.default
-      //   }
-    };
-  },
-  components: {
-    TheaterHeader,
-    Loading,
-    Opening,
-    Scence,
-    Procedure,
-    TheaterOptions
-    // Lottie
-  },
-  created() {
-    // 實體化
-    this.ScenceManger = SceneManager(scenesAll);
-  },
-  mounted() {
-    // 第一場景開始
-    this.ScenceManger.createStart();
-  },
+import {
+    SceneManager
+} from "@/plugins/scenceFactory.js";
+import {
+    scenesAll
+} from "@/static/json/scenes.js";
+import {
+    mapGetters,
+    mapActions
+} from 'vuex';
 
-  methods: {
-    openingBtn() {
-      this.isShow = "Scence";
+export default {
+    name: "Home",
+    data() {
+        return {
+            ScenceManger: {},
+            isShow: "Opening",
+            // 測試
+            // isShow: "Scence",
+            isTranform: false
+            //   defaultOptions: {
+            //     animationData: animationData.default
+            //   }
+        };
     },
-    // 下一頁
-    nextScence(sIds) {
-      this.isShow = "";
-      this.ScenceManger.next(sIds, scenesAll);
-      this.$nextTick(() => {
-        this.isShow = "Scence";
-      });
+    components: {
+        TheaterHeader,
+        Loading,
+        Opening,
+        Scence,
+        Procedure,
+        TheaterOptions
+        // Lottie
+    },
+    created() {
+        // 實體化
+        // this.ScenceManger = SceneManager(scenesAll);
+        this.init(scenesAll, 8);
+    },
+    mounted() {
+        // 第一場景開始
+        // this.ScenceManger.createStart();
+        this.createStart();
+    },
+    computed: {
+        ...mapGetters('ScenceManger', ['getCurrentData', 'getScenceDataById']),
+    },
+    methods: {
+        ...mapActions('ScenceManger', ['nextScence', 'init', 'createStart']),
+        openingBtn() {
+            this.isShow = "Scence";
+        },
+        // 下一頁
+        nextScence(sIds) {
+            this.isShow = "";
+            this.ScenceManger.nextScence(sIds);
+
+            this.$nextTick(() => {
+                this.isShow = "Scence";
+            });
+        }
+        // ani() {
+        //   this.defaultOptions.animationData = animation2.default;
+        // }
     }
-    // ani() {
-    //   this.defaultOptions.animationData = animation2.default;
-    // }
-  }
 };
 </script>
 
 <style lang="scss">
 .Home {
-  width: 100%;
-  max-width: var(--scenes_w);
-  margin: 0 auto;
-}
-.theater {
-  position: relative;
-  &__pcCover {
-    @include md-media {
-      height: var(--scenes_h);
-      overflow: hidden;
-    }
-  }
-  &__main {
-    position: relative;
     width: 100%;
-    // height: calc(1000 / 100 * 670);
-    min-height: var(--scenes_h);
-    background: var(--Home-bg);
-    box-shadow: 0 0 15px var(--box-shadow);
-    overflow: hidden;
-  }
+    max-width: var(--scenes_w);
+    margin: 0 auto;
+}
+
+.theater {
+    position: relative;
+
+    &__pcCover {
+        @include md-media {
+            height: var(--scenes_h);
+            overflow: hidden;
+        }
+    }
+
+    &__main {
+        position: relative;
+        width: 100%;
+        // height: calc(1000 / 100 * 670);
+        min-height: var(--scenes_h);
+        background: var(--Home-bg);
+        box-shadow: 0 0 15px var(--box-shadow);
+        overflow: hidden;
+    }
 }
 
 // 開場、場景淡出淡入
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 1s;
+    transition: opacity 1s;
 }
+
 .fade-enter,
 .fade-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 
 // 轉場淡出淡入
 .trans-enter-active {
-  transition: opacity 0.2s;
+    transition: opacity 0.2s;
 }
+
 .trans-leave-active {
-  transition: opacity 1s;
+    transition: opacity 1s;
 }
+
 .trans-enter,
 .trans-leave-to {
-  opacity: 0;
+    opacity: 0;
 }
 </style>
