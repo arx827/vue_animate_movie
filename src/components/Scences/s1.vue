d532669a37acdd17c4580f1d1f53678959260617
Eason Chen <eason.yc.chen@fubon.com>
2020-10-26 14:43:04 +0800

    新增 場景 - s1

--- src/components/Scences/s1.vue
+++ src/components/Scences/s1.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="d-flex justify-content-center align-items-center h-100">
-    測試s1
+  <div class="lottie">
+    <lottie :options="defaultOptions" />
   </div>
 </template>
+
+<script>
+import Lottie from '@/components/Lottie.vue';
+import * as loadingData from '@/lotties/s1/data.json';
+
+export default {
+  name: 'Loading',
+  data() {
+    return {
+      defaultOptions: {
+        animationData: loadingData.default,
+        pathId: 's1'
+      },
+    };
+  },
+  components: {
+    lottie: Lottie,
+  }
+};
+</script>
